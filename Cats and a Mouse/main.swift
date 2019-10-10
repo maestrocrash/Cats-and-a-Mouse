341b25749575d48ce0b64e4289b2112948372cac
maestrocrash <maestrocrash@gmail.com>
2019-10-10 13:39:16 +0300

    Solution to problem

--- Cats and a Mouse/main.swift
+++ Cats and a Mouse/main.swift
@@ -8,5 +8,23 @@
 
 import Foundation
 
-print("Hello, World!")
+func catAndMouse(x: Int, y: Int, z: Int) -> String {
+
+    var s: String
+
+    switch z {
+    case _ where abs(x - z) > abs(y - z):
+        s = "Cat B"
+    case _ where abs(x - z) < abs(y - z):
+        s = "Cat A"
+    default:
+        s = "Mouse C"
+    }
+     
+    return s
+}
+
+print(catAndMouse(x: 1, y: 2, z: 3))
+print(catAndMouse(x: 1, y: 3, z: 2))
+
 
