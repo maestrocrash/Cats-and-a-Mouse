ded6ef1caa53c48a3948cfc6bb809bc52bd6cecc
maestrocrash <maestrocrash@gmail.com>
2019-10-10 12:54:16 +0300

    Initial Commit

---
+++ Cats and a Mouse/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Cats and a Mouse
+//
+//  Created by Shakhvorostov on 10.10.2019.
+//  Copyright © 2019 Shakhvorostov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
